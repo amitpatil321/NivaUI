065f38539c107e57b568da3540585dc93a098fc5
Amit <amitpatil321@gmail.com>
2025-09-23 20:23:00 +0530

    feat: add tailwind initial commit

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -1,10 +1,13 @@
 /** @type { import('@storybook/react-vite').Preview } */
+
+import "./tailwind.css";
+
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
